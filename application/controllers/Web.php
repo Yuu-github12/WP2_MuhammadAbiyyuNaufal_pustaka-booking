f2e6228c3ba852ace719804766ff7fd86b5e31c8
Yuu-github12 <SHNV1221@gmail.com>
2023-03-30 19:53:20 +0700

    menambahkan about

--- application/controllers/Web.php
+++ application/controllers/Web.php
@@ -15,7 +15,7 @@ class Web extends CI_Controller{
         $this->load->view('v_footer',$data);
     }
     public function about(){
-        $data['judul'] = "Halaman Depan";
+        $data['judul'] = "Halaman About";
         $this->load->view('v_header',$data);
         $this->load->view('v_about', $data);
         $this->load->view('v_footer',$data);
