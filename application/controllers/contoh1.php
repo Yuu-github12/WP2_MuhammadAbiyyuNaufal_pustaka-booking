6fe788bdf49bb978984399a44ad982cd84317090
Yuu-github12 <SHNV1221@gmail.com>
2023-03-30 08:41:09 +0700

    menambakan latihan Contoh1 dari folder pustaka-booking

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,14 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Perkenalkan nama saya Muhammad Abiyyu Naufal. 
+        Nim saya adalah 17200071.
+        Saya berada di kelas 17.6A.01 .
+        Saya tinggal di Kota Depok.
+        Hobi saya adalah
+        mengotak-atik komputer";
+    }
+}
